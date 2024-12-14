9655456fd9cf10cfbb217b419c11497bca0c4309
maibenbenYegor <egorka.khvostov@mail.ru>
2024-12-14 14:02:14 +0300

    refactor gameprocess and level, add new option to level: amount constraint: level can be without timer, but with amount constraint

--- modules/gameprocess.py
+++ modules/gameprocess.py
@@ -1,128 +1,184 @@
-import os
-import sys
 import asyncio
+import os
 import random
+import sys
 import time
-from .util import create_stdin_reader
+from dataclasses import dataclass
+
 from .exceptions import Escape
 
+
 class GameProcess:
-    def __init__(self, reader, *,
-                 duration,
-                 dictionary):
-        self._duration = duration
-        self._dict = dictionary
-        self._reader = reader
+    reader: asyncio.StreamReader
+    dct: list[tuple[str, list[str]]]
+    fails: int
+    points: int
+    order: list[int]
+    fail_words: dict[str, list[str]]
+
+    duration: None | int
+    amount: None | int
+
+    tasks: list[asyncio.Task[str]]
+    done: set[asyncio.Task[str]]
+    pend: set[asyncio.Task[str]]
+
+    word: str
+    translates: list[str]
+
+    def __init__(self, reader, *, duration, dictionary, amount=None):
+        self.duration = duration
+        self.dct = dictionary
+        self.amount = amount
+        self.reader = reader
         self.fails = 0
         self.points = 0
+        self.fail_words = {}
+        self.order = list(range(len(self.dct)))
+        self.init_drawers()
 
-    async def start(self):
-        self._draw_msg = Drawer(1, 1).draw
-        self._draw_msg2 = Drawer(1, 2).draw
-        self._draw_time = Drawer(1, 3).draw
-        self._draw_word = Drawer(1, 4).draw
-        self._read_word = InputReader(1, 6, self._reader).read
-        self._draw_translates = Drawer(1, 9).draw
+    def init_drawers(self):
+        self.draw_msg = Drawer(1, 1).draw
+        self.draw_msg2 = Drawer(1, 2).draw
+        self.draw_time = Drawer(1, 3).draw
+        self.draw_word = Drawer(1, 4).draw
+        self.read_word = InputReader(1, 6, self.reader).read
+        self.draw_translates = Drawer(1, 9).draw
 
-        self._fail_words = {}
+    def init_tasks(self):
+        process_task = asyncio.create_task(self.process())
+        self.tasks = [process_task]
+        if self.duration:
+            timer_task = asyncio.create_task(self.timer())
+            self.tasks.append(timer_task)
 
-        tasks = [t_task := asyncio.create_task(self._timer()),
-                 p_task := asyncio.create_task(self._process())]
-        done, pending = await asyncio.wait(tasks,
-                           return_when=asyncio.FIRST_COMPLETED)
-        [task.cancel() for task in pending]
+    async def run(self):
+        self.init_tasks()
+        await self.wait_first_complete_task()
+        self.cancel_pending_tasks()
+        await self.end_notify()
 
-        if (exited := done.pop().result()) == 'timeout':
-            os.system('clear')
-            print("TIME'S OUT")
-            await asyncio.sleep(2)
-        else:
-            await asyncio.sleep(0)
+        return GameResult(
+            points=self.points,
+            fails=self.fails,
+            exited=self.exited,
+            failed_words=self.fail_words,
+        )
 
-        return {
-            'points': self.points,
-            'fails': self.fails,
-            'exited': exited,
-            'failed_words': self._fail_words,
-        }
+    async def wait_first_complete_task(self):
+        self.done, self.pend = await asyncio.wait(
+            self.tasks, return_when=asyncio.FIRST_COMPLETED
+        )
 
+    def cancel_pending_tasks(self):
+        [task.cancel() for task in self.pend]
 
-    async def _timer(self):
-        start = int(time.time()) + self._duration
-        count = self._duration
+    async def end_notify(self):
+        os.system("clear")
+        match exited := self.done.pop().result():
+            case "timeout":
+                print("TIME'S OUT")
+            case "words out":
+                print("Good Job!!!")
+            case _:
+                print("ESCAPED")
+        await asyncio.sleep(0)
+        await self.reader.read(4)
+        self.exited = exited
+
+    async def timer(self) -> str:
+        if not self.duration:
+            raise Escape()
+        start = int(time.time()) + self.duration
+        count = self.duration
         while count > 0:
-            self._draw_time(f'Time: {count}')
+            self.draw_time(f"Time: {count}")
             await asyncio.sleep(1)
             count = start - int(time.time())
-        return 'timeout'
+        return "timeout"
 
-
-    async def _check(self, en_word, translates):
-        old_fails = self.fails
-
-        while (await self._read_word()) not in translates:
-            self.fails += 1
-            self._draw_translates(f'possible translates: {translates}')
-            self._draw_msg('Fails++')
-            self._draw_msg2(f'points: {self.points} / fails: {self.fails}')
-            self._fail_words[en_word] = translates
-
-        if old_fails == self.fails:
+    def on_success(self):
+        if self.old_fails == self.fails:
             self.points += 1
+            self.draw_msg("Points++")
+            self.draw_translates(f'"{self.word}" translates: {self.translates}')
+            self.draw_msg2(f"points: {self.points} / fails: {self.fails}")
+            if self.is_game_over():
+                raise WordsOut()
 
+    def on_fail(self):
+        self.fails += 1
+        self.fail_words[self.word] = self.translates
+        self.draw_translates(f"possible translates: {self.translates}")
+        self.draw_msg("Fails++")
+        self.draw_msg2(f"points: {self.points} / fails: {self.fails}")
 
-    async def _process(self):
-        os.system('clear')
-        order = list(range(len(self._dict)))
-        self._draw_msg2(f'points: {self.points} / fails: {self.fails}')
+    async def play_word_until_success(self, index):
+        self.word, self.translates = self.dct[index]
+        self.old_fails = self.fails
+        self.draw_word(self.word)
+        while await self.read_word() not in self.translates:
+            self.on_fail()
+        self.on_success()
+
+    async def play_round(self):
+        for i in self.order:
+            await self.play_word_until_success(i)
+
+    def reshufle_words_order(self):
+        random.shuffle(self.order)
+
+    async def process(self) -> str:
+        os.system("clear")
+        self.draw_msg2(f"points: {self.points} / fails: {self.fails}")
         try:
             while True:
-                random.shuffle(order)
-                for i in order:
-                    word, translates = self._dict[i]
-                    self._draw_word(word)
-                    await self._check(word, translates)
-                    self._draw_msg('Point++')
-                    self._draw_msg2(f'points: {self.points} / '
-                                    f'fails: {self.fails}')
-                    self._draw_translates(f'previous translates: {translates}')
+                self.reshufle_words_order()
+                await self.play_round()
+        # when level with amount constraint (points == amount)
+        except WordsOut:
+            return "words out"
+        # when user press 'esc'
         except Escape:
-            return 'escape'
+            return "escape"
+
+    def is_game_over(self):
+        return (self.points >= self.amount) if self.amount else False
 
 
 class Drawer:
     def __init__(self, x, y):
-        self._x = x
-        self._y = y
+        self.x = x
+        self.y = y
 
     def draw(self, string):
-        sys.stdout.write('\0337')
-        sys.stdout.write(f'\033[{self._y};{self._x}H')
-        sys.stdout.write('\033[K')
+        sys.stdout.write("\0337")
+        sys.stdout.write(f"\033[{self.y};{self.x}H")
+        sys.stdout.write("\033[K")
         sys.stdout.write(string)
-        sys.stdout.write('\0338')
+        sys.stdout.write("\0338")
         sys.stdout.flush()
 
 
 class InputReader:
     def __init__(self, x, y, reader):
-        self._x = x
-        self._y = y
-        self._reader = reader
+        self.x = x
+        self.y = y
+        self.reader = reader
 
     async def read(self):
-        sys.stdout.write(f'\033[{self._y};{self._x}H')
-        sys.stdout.write('\033[K')
+        sys.stdout.write(f"\033[{self.y};{self.x}H")
+        sys.stdout.write("\033[K")
         sys.stdout.flush()
         chars = []
-        while (char := await self._reader.read(4)) != b'\n':
-            if char == b'\x7F' and chars:
+        while (char := await self.reader.read(4)) != b"\n":
+            if char == b"\x7F" and chars:
                 chars.pop()
-                sys.stdout.write('\033[D')
-                sys.stdout.write(' ')
-                sys.stdout.write('\033[D')
+                sys.stdout.write("\033[D")
+                sys.stdout.write(" ")
+                sys.stdout.write("\033[D")
                 sys.stdout.flush()
-            elif char == b'\x1B':
+            elif char == b"\x1B":
                 raise Escape()
             else:
                 char = char.decode()
@@ -130,7 +186,16 @@ class InputReader:
                 sys.stdout.write(char)
                 sys.stdout.flush()
 
+        return "".join(chars).strip()
 
-        return ''.join(chars).strip()
+
+@dataclass
+class GameResult:
+    points: int
+    fails: int
+    exited: str
+    failed_words: dict[str, list[str]]
 
 
+class WordsOut(Exception):
+    pass
--- modules/level.py
+++ modules/level.py
@@ -1,92 +1,96 @@
-from .menu import Menu, EscapeMenu
-from .gameprocess import GameProcess
-from .exceptions import Escape
-from random import randrange
-from settings import DICT_DIR
 from pathlib import Path
+from random import randrange
+
+from settings import DICT_DIR
+
+from .gameprocess import GameProcess, GameResult
+from .menu import EscapeMenu, Menu
+
 
 class EmptyDict(Exception):
     pass
 
+
 class Level(Menu):
-    def __init__(self,
-         title, *,
-         range_start=0,
-         range_num=None,
-         duration=None,
-         amount=None,
-         dictionary = None
-     ):
+
+    def __init__(
+        self,
+        title,
+        *,
+        range_start=0,
+        range_num=None,
+        duration=None,
+        amount=None,
+        dictionary=None,
+        constraint=None,
+    ):
         super().__init__(title)
         self._range_start: int = range_start or 0
         self._range_num: int | None = range_num or None
         self._amount: int | None = amount or None
-        self._dictionary: set | tuple | list | dict = dictionary
+        self._dictionary: list[tuple[str, list[str]]] | None = dictionary
         self._opts = {
-            'duration': duration or 60,
-            'dictionary': None,
+            "amount": constraint,
+            "duration": duration if constraint or duration else 60,
         }
-
-
-    async def _start(self):
-        try:
-            self._opts['dictionary'] = self._get_dict()
-            gameprocess = GameProcess(self._reader, **self._opts)
-            results = await gameprocess.start()
-
-
-            self._menu_list = [
-                EscapeMenu('back'),
-                Menu(''),
-                Menu('Results:'),
-                Menu(f'exited: {results["exited"]}'),
-                Menu(f'points: {results["points"]}'),
-                Menu(f'fails: {results["fails"]}'),
-            ]
-
-            if results["fails"]:
-                fails_level = Level('play with fails',
-                    dictionary=results['failed_words'])
-                fails_level.set_parent(self._parent)
-                self._menu_list.insert(0, fails_level)
-
-        except EmptyDict:
-            self._menu_list = [
-                Menu(f'empty dictionary:  INCORRECT level settings!'),
-                EscapeMenu('back'),
-            ]
-
-    def _get_dict(self) -> list | tuple:
-        if isinstance(self._dictionary, dict):
-            return tuple(self._dictionary.items())
-        elif isinstance(self._dictionary, (list, tuple)):
-            return self._dictionary
-        elif isinstance(self._dictionary, set):
-            return tuple(self._dictionary)
-
-        lines = self._load_lines()
-        cliped = self._clip_lines(lines)
-        randomlines = self._random_lines(cliped)
-        result = tuple(map(self._line_to_tuple, randomlines))
-        return result
-
-
-    async def on_escape(self):
-        self._menu_list = []
-
 
     async def run(self):
         await self._start()
 
+    async def on_escape(self):
+        self._menu_list = []
 
-    def _load_lines(self) -> list[str]:
+    async def _start(self):
+        try:
+            self._opts["dictionary"] = self._get_dict()
+            gameprocess = GameProcess(self._reader, **self._opts)
+            result: GameResult = await gameprocess.run()
+
+            if fails := result.fails:
+                constraint = fails * 2
+                fails_level = Level(
+                    "play with fails",
+                    dictionary=list(result.failed_words.items()),
+                    constraint=constraint,
+                )
+                self._menu_list.append(fails_level)
+                fails_level.set_parent(self._parent)
+
+            self.add_submenu(
+                EscapeMenu("back"),
+                Menu(""),
+                Menu("Results:"),
+                Menu(f"exited: {result.exited}"),
+                Menu(f"points: {result.points}"),
+                Menu(f"fails: {result.fails}"),
+            )
+
+        except EmptyDict:
+            self._menu_list = [
+                Menu("empty dictionary:  INCORRECT level settings!"),
+                EscapeMenu("back"),
+            ]
+
+    def _get_dict(self) -> list[tuple[str, list[str]]]:
+        if self._dictionary:
+            return self._dictionary
+
+        lines = self._read_lines_from_dict_file()
+        cliped_lines = self._clip_lines(lines)
+        random_lines = self._random_lines(cliped_lines)
+        list_of_tuples_dict = self._lines_to_tuple_list(random_lines)
+        return list_of_tuples_dict
+
+    def _lines_to_tuple_list(self, lines: list[str]) -> list[tuple[str, list[str]]]:
+        return list(map(self._line_to_tuple, lines))
+
+    def _read_lines_from_dict_file(self) -> list[str]:
         mode = self._parent.get_parent().title
         dct = self._parent.title
         with open(Path(DICT_DIR) / mode / dct) as f:
             return f.readlines()
 
-
-    def _random_lines(self, lines: list[tuple[str, tuple[str]]]) -> set[tuple[str, tuple[str]]]:
+    def _random_lines(self, lines: list[str]) -> list[str]:
         if not self._amount or self._amount > len(lines):
             return lines
 
@@ -94,35 +98,42 @@ class Level(Menu):
         while len(result) < min(self._amount, len(lines)):
             num = randrange(0, len(lines))
             result.add(lines[num])
-        return result
+        return list(result)
 
+    def _line_to_tuple(self, line: str) -> tuple[str, list[str]]:
+        eng, ru = line.split("=")
+        ru = list(map(lambda w: w.strip(), ru.split(",")))
+        return eng.strip(), ru
 
-    def _line_to_tuple(self, line: str) -> tuple[str, tuple[str]]:
-        eng, ru = line.split('=')
-        ru = tuple(map(lambda w: w.strip(), ru.split(',')))
-        return (eng.strip(), ru)
-
-
-    def _clip_lines(self, lines: list) -> list:
-        cliped = lines[self._range_start:][:self._range_num]
+    def _clip_lines(self, lines: list[str]) -> list[str]:
+        cliped = lines[self._range_start :][: self._range_num]
         if not cliped:
             raise EmptyDict()
         return cliped
 
+
 class DirLevel(Level):
-    def __init__(self, title, dir, subtitle='', *,
-                 range_start=0,
-                 range_num=None,
-                 duration=60,
-                 amount=None):
-        super().__init__(title + ' ' + subtitle,
-                         range_start=range_start,
-                         range_num=range_num,
-                         duration=duration,
-                         amount=amount)
+    def __init__(
+        self,
+        title,
+        dir,
+        subtitle="",
+        *,
+        range_start=0,
+        range_num=None,
+        duration=60,
+        amount=None,
+    ):
+        super().__init__(
+            title + " " + subtitle,
+            range_start=range_start,
+            range_num=range_num,
+            duration=duration,
+            amount=amount,
+        )
         self._dir: str = dir
         self._filename: str = title
+
     def _load_lines(self) -> list[str]:
         with open(f"dicts/{self._dir}/{self._filename}") as f:
             return f.readlines()
-
